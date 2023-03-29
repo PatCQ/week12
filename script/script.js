675a178f522319c3adc7b45e0ff1f278fa954bca
Patrick Q <pquach3@my.bcit.ca>
2023-03-29 14:11:02 -0700

    Slide 7 - clicking button changes background to pink and toggles the text

--- script/script.js
+++ script/script.js
@@ -2,8 +2,21 @@
 //2. define a function to alert the user
 //3. add the function as event listener
 
-let btn = document.querySelector("button");
+const btn = document.querySelector("button");
 function buttonPressed() {
     alert("Button pressed");
 }
+function changeBGPink() {
+    document.body.classList.add("pinkBG");
+}
+function changeText() {
+    if (btn.textContent == "Clicked!") {
+        btn.textContent = "Click me!";
+    }
+    else {
+        btn.textContent = "Clicked!"
+    }
+}
 btn.addEventListener("click", buttonPressed, {once: true});
+btn.addEventListener("click", changeBGPink, {once: true});
+btn.addEventListener("click", changeText);
